d8dbd8700c03e434a7b8af173b08558fc298f36b
Jonathan Chaney <jonchaney@gmail.com>
2018-02-20 13:16:53 -0800

    orm challenge 3 done

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,19 +1,17 @@
 class Category < ApplicationRecord
     has_and_belongs_to_many :products
 
-     # customer_id customer_first_name category_id category_name number_purchased
-    # output example: 47 Maudi Audio 6
     def items_bought_per_category
-        products_per_category = Product.select('products.id, categories.id, categories.name')
-                                       .joins('JOIN categories_products ON products.id = categories_products.product_id')
-                                       .joins('JOIN categories ON categories.id = categories_products.category_id').to_sql
+        sql = User.select('users.id AS user_id, users.first_name, categories.id AS category_id, categories.name, COUNT(products.id)')
+        .joins('JOIN orders ON users.id = orders.user_id')
+        .joins('JOIN orders_products ON orders.id = orders_products.order_id')
+        .joins('JOIN products ON orders_products.product_id = products.id')
+        .joins('JOIN categories_products ON orders_products.product_id = categories_products.product_id')
+        .joins('JOIN categories ON categories_products.category_id = categories.id ')
+        .group('users.id, categories.id, categories.name')
+        .order('users.id').to_sql
 
-        products_per_user = User.select('users.id, orders.id')
-                                .joins('JOIN orders ON users.id = orders.user_id')
-                                .joins('JOIN orders_products ON order.id = orders_products.order_id').to_sql
-
-        products_per_category = Product.select('users.id, users.first_name, categories.id, category.name, COUNT(products.id)')
-                                       .joins()
+        ActiveRecord::Base.connection.exec_query(sql)
     end 
 end
 
--- sql/challenge_3.sql
+++ sql/challenge_3.sql
@@ -1,7 +1,7 @@
 SELECT 
-    users.id, users.first_name, categories.id, categories.name, COUNT(products.id) 
+    users.id AS user_id, users.first_name, categories.id AS category_id, categories.name, COUNT(products.id) 
 FROM 
-    users 
+    users
 JOIN 
     orders ON users.id = orders.user_id 
 JOIN 
@@ -16,3 +16,5 @@ GROUP BY
     users.id, categories.name, categories.id 
 ORDER BY 
     users.id;
+
+SELECT users.id AS user_id, users.first_name, categories.id AS category_id, categories.name, COUNT(products.id)  FROM users JOIN orders ON users.id = orders.user_id JOIN orders_products ON orders.id = orders_products.order_id JOIN products ON orders_products.product_id = products.id JOIN categories_products ON orders_products.product_id = categories_products.product_id JOIN categories ON categories_products.category_id = categories.id GROUP BY users.id, categories.name, categories.id ORDER BY users.id;
